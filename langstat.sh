a231e3985016c4f878315a0c3ff7eb1b9c97ee69
sebastienmm <sebastien_meylan@orange.fr>
2017-01-13 00:26:38 +0100

    Ecriture de la fonction de recherche du nombre de mots par lettre présentes

--- langstat.sh
+++ langstat.sh
@@ -4,25 +4,35 @@ fichier_texte=$1
 
 verif_exist_param1(){
 
-if [ -z $fichier_texte ]
-then		
-	read -p " Entrez un fichier texte à analiser pour effectuer une statistique :" $fichier_texte 
-else
-	echo ' le paramètre $1 est renseigné '
+	if [ -z $fichier_texte ]
+	then		
+		read -p " Entrez un fichier texte à analiser pour effectuer une statistique :" $fichier_texte 
+	else
+		echo ' le paramètre $1 est renseigné '
 	
-fi
+	fi
 
 }
+
+
 verif_exist_fichier(){
 
-if [ -e $fichier_texte ]
-then
-	echo " Le fichier existe,nous pouvons continuez "
-else
-	read  -p " Le fichier $fichier_texte n'existe pas veuillez entrez un autre nom :" fichier_texte 
-	verif_exist_fichier
-fi
+	if [ -e $fichier_texte ] && [ -f $fichier_texte ]
+	then
+		echo " Le fichier existe,nous pouvons continuez "
+	else
+		read  -p " Le fichier $fichier_texte n'existe pas veuillez entrez un autre nom :" fichier_texte 
+		verif_exist_fichier
+	fi
 }
 verif_exist_param1
 verif_exist_fichier
 
+recherche_nombre_mots(){
+	for lettre in {A..Z}	
+	do
+		echo "$(grep $lettre $fichier_texte | wc -l) : $lettre" >> stat.txt
+	done
+	sort -n stat.txt
+}
+recherche_nombre_mots
