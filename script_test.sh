5a3f801a44cf5dea4a49aba8556b85903e0c0056
sebastienmm <sebastien_meylan@orange.fr>
2017-01-25 16:40:27 +0100

    Ecriture du script permettant de rechercher un palindrome dans un texte et modification du fichier texte_test.txt

--- script_test.sh
+++ script_test.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-texte=`cat texte_test.txt`
-echo "$texte"
+fichier_analyse=$2
 
-for mot in $texte
+palindrome(){
+compt=0
+for lettre in {A..Z}
 do
-		tom=`echo $mot | rev`
-		
+mot_trie=$(sed 's/ /\n/g' $fichier_analyse | grep -i "\<$lettre"  | sed 's/[\r]*$//' )
+echo -e " Pour la lettre $lettre " >> palindrome.txt
+	for mot in $mot_trie
+	do	
+			tom=$(echo "$mot" | rev)
+			#echo "$tom"
 		 
-		if [ $mot = $tom ]
-		then
-			echo " $mot " >> palindrome.txt
-		fi
+			if [ $mot = $tom ]
+			then
+				#echo "$mot"
+				echo  "$mot" >> palindrome.txt
+			fi
+			compt=$((compt + 1))
+			echo "$compt"
+	done
+echo  "  " >> palindrome.txt
 done
 
-
-chaine=`cat palindrome.txt`
-if [ -n chaine ]
+chaine=$(cat palindrome.txt)
+if [ -n "$chaine" ]
 then
 	echo -e " La liste des palindromes du texte sont :\n $chaine "
 else
-	echo -e " Il n\'y a pas e palindrome dans ce texte "
+	echo -e " Il n\'y a pas de palindrome dans ce texte "
 fi
+}
 
+palindrome
 
